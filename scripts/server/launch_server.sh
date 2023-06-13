09a2da36115d9dfb46f5164bd719706dbcce8a02
r2d2 <kirsty.ellis@mila.quebec>
2023-06-13 12:42:55 -0400

    nuc updates

--- scripts/server/launch_server.sh
+++ scripts/server/launch_server.sh
@@ -1,4 +1,8 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate polymetis-local
-cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-python run_server.py
+sudo ip link set enp89s0 down
+sudo ip link set enp89s0 up
+sudo service network-manager restart
+
+#cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+#python run_server.py
